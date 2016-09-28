3f4d800c478fbbaa8bbb9cacebf71fbbee4a2202
Hasan Ahmed Khan <hasan.khan.uap@gmail.com>
2016-09-28 13:10:37 -0700

    New

--- BrainEngine_v0.0.1/DP/DataProcessor.py
+++ BrainEngine_v0.0.1/DP/DataProcessor.py
@@ -71,15 +71,21 @@ def getAllTextFromPageContent(html):
     soup = BeautifulSoup(html,"html.parser") # create a new bs4 object from the html data loaded
     for script in soup(["script", "style"]): # remove all javascript and stylesheet code
         script.extract()
-    # get text
+
+    # get all text
     text = soup.get_text()
+
     # break into lines and remove leading and trailing space on each
     lines = (line.strip() for line in text.splitlines())
+
     # break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
     # drop blank lines
     all_text = '\n'.join(chunk for chunk in chunks if chunk)
-    return all_text
+
+    print(all_text)
+    #return all_text
 
 startDataProcessor()
 
