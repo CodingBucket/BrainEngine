746a2fbcf18e6b6d0be7d9cacc0a5c24c44c0ee1
Hasan Ahmed Khan <hasan.khan.uap@gmail.com>
2016-10-17 22:37:48 -0700

    SA

--- BrainEngine_v0.0.1/DR/SA/SearchAlgorithom.py
+++ BrainEngine_v0.0.1/DR/SA/SearchAlgorithom.py
@@ -9,29 +9,30 @@ import DR.DataRetriverModel as DPModel
 
 def getData():
 
-    search_query = 'stack'
+    search_query = 'stack'                           # For testing
 
-    # Title based filter
-    page_title = DPModel.getTitle(search_query)
+    # Get page info from pages Table
+    page_title = DPModel.getTitle(search_query)      # Title based filter
     pprint.pprint(page_title)
     print('-----------------')
 
-    # Doc count based filter
-    doc = DPModel.getDoc(search_query)
-    pprint.pprint(doc)
-    pprint.pprint(doc[0][2])
+    # Get doc from page_docs Table
+    doc = DPModel.getDoc(search_query)               # Doc count based filter
 
+    # Assign page doc count in variable
     page_doc_count_string = doc[0][2]
 
-    # String to dictionary convert
+    # String to dictionary convertion
     page_doc_count_dic = ast.literal_eval(page_doc_count_string)
-    pprint.pprint(page_doc_count_dic['1'])
-
+    pprint.pprint(page_doc_count_dic)
     print('-----------------')
 
-    # Append page_doc_count in page_title tuple
-    page_title = page_title + (33,)
-    pprint.pprint(page_title)
+    page_info_with_doc_count = []                    # Initialize a tuple
+    for page_info in page_title:                     # Loop page info
+        page_id = page_info[0]
+        # Append page and doc_count in page_info_with_doc_count tuple
+        page_info_with_doc_count = page_info + (page_doc_count_dic[str(page_id)],)
+        print(page_info_with_doc_count)
     print('-----------------')
 
 getData()
