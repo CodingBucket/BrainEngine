6348c9729267660afa7d9549681be41e28b86d9d
Hasan Ahmed Khan <hasan.khan.uap@gmail.com>
2016-10-17 21:38:14 -0700

    SA

--- BrainEngine_v0.0.1/DR/DataRetriverModel.py
+++ BrainEngine_v0.0.1/DR/DataRetriverModel.py
@@ -32,14 +32,14 @@ def db_query(sql, query_type):
 
 # Get data from pages Table
 def getTitle(search_query):
-    sql = " SELECT page_link, page_title FROM pages WHERE page_title LIKE '%s' " % ("%"+search_query+"%")
+    sql = " SELECT page_id, page_link, page_title FROM pages WHERE page_title LIKE '%s' " % ("%"+search_query+"%")
     query_type = 'select'
     result = db_query(sql,query_type)
     return result
 
 # Get doc from page_docs Table
-def getTitle(search_query):
-    sql = " SELECT page_link, page_title FROM pages WHERE page_title LIKE '%s' " % ("%"+search_query+"%")
+def getDoc(search_query):
+    sql = " SELECT * FROM page_docs WHERE doc_index LIKE '%s' " % ("%"+search_query+"%")
     query_type = 'select'
     result = db_query(sql,query_type)
     return result
--- BrainEngine_v0.0.1/DR/SA/SearchAlgorithom.py
+++ BrainEngine_v0.0.1/DR/SA/SearchAlgorithom.py
@@ -13,8 +13,16 @@ def getData():
     # Title based filter
     page_title = DPModel.getTitle(search_query)
     pprint.pprint(page_title)
+    print('-----------------')
 
     # Doc count based filter
     doc = DPModel.getDoc(search_query)
+    pprint.pprint(doc)
+    print('-----------------')
+
+    # Append page_doc_count in page_title tuple
+    page_title = page_title + (33,)
+    pprint.pprint(page_title)
+    print('-----------------')
 
 getData()
