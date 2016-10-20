7a9c82506dc273c8d3120edff6d1626b22c9281f
Hasan Ahmed Khan <hasan.khan.uap@gmail.com>
2016-10-19 22:05:00 -0700

    SA

--- BrainEngine_v0.0.1/DR/SA/SearchAlgorithom.py
+++ BrainEngine_v0.0.1/DR/SA/SearchAlgorithom.py
@@ -41,6 +41,8 @@ def getData():
     pprint.pprint(page_info_with_doc_count)
     print('-----------------')
 
+    # page_info_with_doc_count = [(2, 'http://stackoverflow.com', 'stack Overflow', 4)]
+    # (page_id, link, page_title, doc_count)
     # page_info_with_doc_count sort by doc_count
     page_info_with_doc_count.sort(key=itemgetter(3), reverse=True)
     pprint.pprint(page_info_with_doc_count)
